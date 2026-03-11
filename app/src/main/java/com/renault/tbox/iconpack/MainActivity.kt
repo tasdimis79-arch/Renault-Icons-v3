b4d99b90c8d635ea3475cf465d3255ae69e3420c
tasdimis79-arch <tasdimis79@gmail.com>
2026-03-11 11:37:41 +0200

    Add files via upload

--- app/src/main/java/com/renault/tbox/iconpack/MainActivity.kt
+++ app/src/main/java/com/renault/tbox/iconpack/MainActivity.kt
@@ -1,47 +1,19 @@
-package com.example.launcher
+package com.renault.tbox.iconpack
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.GridLayout
-import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val grid = GridLayout(this)
-        grid.columnCount = 3
-        grid.rowCount = 6
-
-        val icons = arrayOf(
-            R.drawable.apps,
-            R.drawable.bluetooth,
-            R.drawable.car,
-            R.drawable.chrome,
-            R.drawable.drive,
-            R.drawable.eradio,
-            R.drawable.files,
-            R.drawable.gmail,
-            R.drawable.maps,
-            R.drawable.music,
-            R.drawable.phone,
-            R.drawable.playstore,
-            R.drawable.settings,
-            R.drawable.spotify,
-            R.drawable.torque,
-            R.drawable.weather,
-            R.drawable.wifi,
-            R.drawable.youtube
-        )
-
-        for (icon in icons) {
-            val img = ImageView(this)
-            img.setImageResource(icon)
-            img.setPadding(20,20,20,20)
-            grid.addView(img)
+        val textView = TextView(this).apply {
+            text = "Renault Tbox Icon Pack\n\nInstalled successfully.\n\nOpen your launcher and select:\nIcon Pack -> Renault Tbox Icon Pack"
+            textSize = 18f
+            setPadding(48, 96, 48, 48)
         }
 
-        setContentView(grid)
+        setContentView(textView)
     }
 }
